224a828fad0e02ec92255365c8622ae3286dea0b
HalidaysNewestWizard <garthcarey0862@gmail.com>
2024-03-16 22:09:09 -0600

    The search funtionality is finished as well as the  update reservation

---
+++ TravelLess/Data/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelLess.Data
+{
+    internal class Reservation
+    {
+        public string Code => ReservationCode();
+        public string FlightCode => Flight.Code;
+        public string Airline => Flight.Airline;
+        public string Name { get; set; }
+        public string Citizenship { get; set; }
+        public double Cost => Flight.Price;
+        public bool Active { get; set; } = true;
+        public Flight Flight { get; set; }
+
+        /// <summary>
+        /// Generates a new reservation code for the reservation
+        /// </summary>
+        public string ReservationCode()
+        {
+            Random rng = new Random();
+            int code = rng.Next(0, 100000);
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return letters[rng.Next(letters.Length)] + code.ToString();
+        }
+       
+    }
+}
---
+++ TravelLess/DataManagers/ReservationManager.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TravelLess.Components.Pages;
+using TravelLess.Data;
+
+namespace TravelLess.DataManagers
+{
+    internal class ReservationManager
+    {
+        public List<Reservation> Reservations { get; set; }
+
+
+        public ReservationManager()
+        {
+            Reservations = new List<Reservation>();
+            if (File.Exists(Assembly.GetExecutingAssembly().Location + "../../../../../../../../reservations.csv"))
+            {
+                var lines = File.ReadAllLines(Assembly.GetExecutingAssembly().Location + "../../../../../../../../reservations.csv");
+                FlightManager flights = new FlightManager(); // Used to grab the flight associated with the reservation
+                foreach (var line in lines)
+                {
+                    Reservation reservation = new Reservation();
+                    var data = line.Split(",");
+                    reservation.Name = data[0];
+                    reservation.Citizenship = data[1];
+                    reservation.Flight = flights.Flights.FirstOrDefault(f => f.Code == data[2]);
+                    Reservations.Add(reservation);
+                }
+            }
+        }
+
+        public void CreateReservation(Flight flight, string name, string citizenship)
+        {
+            Console.WriteLine("Data Received: " + name + "   " + citizenship + "   " + flight.Code);
+            Reservation reservation = new Reservation();
+            reservation.Name = name;
+            reservation.Citizenship = citizenship;
+            reservation.Flight = flight;
+            Reservations.Add(reservation);
+            SaveReservations();
+        }
+        public List<Reservation> FindReso(string searchCode, string searchAir,  string searchName )
+        {
+
+            List<Reservation> SearchedReso = new List<Reservation>();
+            foreach (Reservation reservation in Reservations)
+            { 
+                if (reservation.Code == searchCode ||
+                    reservation.Airline == searchAir ||
+                    reservation.Name == searchName)
+                {
+                    SearchedReso.Add(reservation);
+
+
+                }
+                else if (searchAir == null ||
+                         searchCode==null  ||
+                         searchAir == null ) 
+                {
+                    return SearchedReso;
+                }
+
+            }
+            return SearchedReso;
+        }
+
+
+        public void UpdateReservation(Reservation res, string name, string citizenship)
+        {
+
+
+            ; res.Name = name;
+            res.Citizenship = citizenship;
+            string new_Name = "";
+            string new_Cit = "";
+            name.Replace(name, new_Name);
+            citizenship.Replace(citizenship, new_Cit);
+            SaveReservations();
+
+
+        }
+
+
+        public void SaveReservations()
+        {
+            List<string> lines = new();
+            foreach (Reservation reservation in Reservations)
+            {
+                lines.Add(reservation.Name + "," + reservation.Citizenship + "," + reservation.Flight.Code + "," + reservation.Code);
+            }
+            File.WriteAllLines(Assembly.GetExecutingAssembly().Location + "../../../../../../../../reservations.csv", lines.ToArray());
+        }
+    }
+}
